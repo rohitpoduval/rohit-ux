ab3bbc56da6014ff49ab93857879b874c09ddb27
rohitp <rohit.poduval@hotelkeyapp.com>
2022-05-04 14:03:25 +0530

    Search-step

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,34 +1,53 @@
 <template>
 <div class="block">
       
-      <el-date-picker v-model="daterange" valueType="format"></el-date-picker>
-      {{date}}
+      <el-date-picker type="date" v-model="check_in_date" format="DD MMM, YYYY" value-format="YYYY-MM-DD" placeholder="Select a Date"></el-date-picker>
+      <el-date-picker type="date" v-model="check_out_date" format="DD MMM, YYYY" value-format="YYYY-MM-DD" placeholder="Select a Date"></el-date-picker>
+      <el-button type = "primary" size="large" @click="getRates()">Check Availability</el-button> 
+
+       
+
+   
+
+    
+
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+
+
 
 export default {
     name: 'Search',
 
     data(){
         return{
-            date: '',
             check_in_date: '',
-            check_out_date: ''
-           
+            check_out_date: '',
+            rates: ''
         }
     },
 
-
-    methods: {
-        checkRates(){
-           
-           
-           console.log(this.date)
-
+    methods:  {
+        getRates(){
+            axios.get('https://00.us-east-1.hkdev0.hotelkeyapp.com/hk-crs/properties/0f3788b1-5759-4b45-8939-77a975f940ce/mini-rates-with-availability/v3',
+            {
+                params: {
+                check_in_date : this.check_in_date,
+                check_out_date: this.check_out_date
+                }
+            })
+            .then(res => {
+            let data = res.data.rate_details
+            this.rates = data
+            })
+            
         }
     }
+
 }
 </script>
 
